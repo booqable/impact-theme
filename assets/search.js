139ef3047cfce27c9bcd756f6a89f2c307b3fbc6
Serhii Zlobin <serhii@booqable.com>
2024-04-23 17:37:32 +0300

    Fix search error in website's preview mode

--- assets/search.js
+++ assets/search.js
@@ -4,9 +4,14 @@ class Search {
 
     this.selector = {
       search: ".header__search",
+      searchForm: "#search",
       searchOpener: "#search-opener",
       searchInput: ".header__search-input",
       searchReset: ".header__search-reset"
+    }
+
+    this.attr = {
+      action: "action"
     }
 
     this.params = {
@@ -24,6 +29,7 @@ class Search {
   }
 
   elements() {
+    this.searchForm = this.search.querySelector(this.selector.searchForm);
     this.searchOpener = this.search.querySelector(this.selector.searchOpener);
     this.searchInput = this.search.querySelector(this.selector.searchInput);
     this.searchReset = this.search.querySelector(this.selector.searchReset);
@@ -35,6 +41,7 @@ class Search {
     document.addEventListener("click", this.searchOpen.bind(this));
     document.addEventListener("click", this.searchClear.bind(this));
     document.addEventListener("click", this.searchClose.bind(this));
+    document.addEventListener("submit", this.searchHandleSubmit.bind(this));
   }
 
   // set focus to the input field when opening the search popup
@@ -73,6 +80,22 @@ class Search {
     if (!query) return false;
 
     this.searchInput.value = query;
+  }
+
+  searchHandleSubmit (e) {
+    const target = e.target;
+
+    if (target !== this.searchForm) return false;
+
+    e.preventDefault();
+
+    const value = this.searchInput.value,
+          route = this.searchForm.getAttribute(this.attr.action);
+
+    this.url.href = this.url.origin + route;
+    this.url.searchParams.set(this.params.q, value);
+
+    window.location.href = this.url.href;
   }
 }
 
